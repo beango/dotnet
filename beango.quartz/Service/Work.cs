203afd0218930c8e951c083e87bbd76ca122da48
beango <6588617@gmail.com>
2014-12-04 13:46:06 +0800

    Signed-off-by: beango <6588617@gmail.com>

--- beango.quartz/Service/Work.cs
+++ beango.quartz/Service/Work.cs
@@ -9,77 +9,24 @@ using beango.util;
 namespace beango.quartz
 {
     /// <summary>
-    /// 短信定时发送任务
+    /// 定时作业，
+    /// DisallowConcurrentExecution禁止并行作业
     /// </summary>
+    [DisallowConcurrentExecution]
     public class SmsIntervalSendWork : IJob
     {
-        private static readonly object syncRoot = new object();
-        
         public void Execute(IJobExecutionContext context)
         {
-            bool lockTaken = false;
-            Monitor.TryEnter(syncRoot, 500, ref lockTaken);
             try
             {
-                List<dynamic> smss = new MessageLogDAL().GetMessageLogList();
-                
-                if (null != smss && smss.Count > 0 && lockTaken)
-                {
-                    SendSms(smss);
-                }
+                //do ...
             }
             catch (Exception ex)
             {
-                JobExecutionException exception = new JobExecutionException(ex)
-                                                      {
-                                                          Source = context.JobDetail.Key.ToString(),
-                                                          UnscheduleFiringTrigger = true
-                                                      };
-                LogHelper.Error(exception);
+                
             }
         }
 
-        public static void SendSms(List<dynamic> msgList)
-        {
-            try
-            {
-                const int NumPer = 100;//接口每次最多发送的消息条数
-                var smsGroup = msgList.GroupBy(item => item.ClientID);
-
-                foreach (var group in smsGroup)//每个Client分批次发送
-                {
-                    int index = 0;
-                    while (index < group.Count())
-                    {
-                        var sendSMS = group.Skip(index).Take(NumPer).ToList();
-                        if (sendSMS.Count > 0)
-                        {
-                            RequestData param = new RequestData
-                                                    {
-                                                        msgList = sendSMS
-                                                    };
-                            ThreadPool.QueueUserWorkItem(new YeionSmsService().SendMultiSms, param);
-                            index += sendSMS.Count;
-                        }
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                LogHelper.Error(e);
-            }
-            finally
-            {
-                try
-                {
-                    Monitor.Exit(syncRoot);
-                }
-                catch (Exception ex)
-                {
-                    LogHelper.Error(ex);
-                }
-            }
-        }
     }
 
 }
